a353078172db4c9ab48a93a236a5f7bab65f3b79
niklaspari <oniklas.pari@gmail.com>
2026-02-13 12:58:30 +0200

    apk lisätty

---
+++ app/build/generated/source/buildConfig/debug/com/example/week5/BuildConfig.java
@@ -0,0 +1,14 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.week5;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.week5";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Field from default config.
+  public static final String API_KEY = "3d915849f5d7eb4247f815d4108695d0";
+}
